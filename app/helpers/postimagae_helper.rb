0e52298a6604d1f7ae9b5a34d50d830e10ba05cb
EC2 Default User <ec2-user@ip-172-31-29-216.ap-northeast-1.compute.internal>
2023-11-06 09:10:50 +0000

    [Finish [4-11]

---
+++ app/helpers/postimagae_helper.rb
@@ -0,0 +1,2 @@
+module PostimagaeHelper
+end
---
+++ test/controllers/postimagae_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class PostimagaeControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
